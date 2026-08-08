8e84dda4ba2aef77c933fc98f131cdda1f38af38
lwhobley <lwhobley@uh.edu>
2026-08-08 06:50:00 -0500

    Keep Wrangler independent of FloorModule dependency cycle

--- packages/api/src/modules/operations/operations.module.ts
+++ packages/api/src/modules/operations/operations.module.ts
@@ -3,13 +3,12 @@ import { OperationsController } from './operations.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { BillingModule } from '../../billing/billing.module';
 import { ChatModule } from '../chat/chat.module';
-import { FloorModule } from '../floor/floor.module';
 import { ExecutionAutopilotService } from './execution-autopilot.service';
 import { WranglerController } from './wrangler/wrangler.controller';
 import { WranglerService } from './wrangler/wrangler.service';
 
 @Module({
-  imports: [PrismaModule, BillingModule, ChatModule, FloorModule],
+  imports: [PrismaModule, BillingModule, ChatModule],
   controllers: [OperationsController, WranglerController],
   providers: [ExecutionAutopilotService, WranglerService],
   exports: [ExecutionAutopilotService, WranglerService],
