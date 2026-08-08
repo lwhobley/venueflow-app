0d7df804e70d1dd751f2f24b3f0ef5fb215223f9
lwhobley <lwhobley@uh.edu>
2026-08-08 09:55:00 -0500

    refactor: register Wrangler operator at app boundary

--- packages/api/src/modules/operations/operations.module.ts
+++ packages/api/src/modules/operations/operations.module.ts
@@ -7,14 +7,12 @@ import { ChatModule } from '../chat/chat.module';
 import { ExecutionAutopilotService } from './execution-autopilot.service';
 import { WranglerController } from './wrangler/wrangler.controller';
 import { WranglerHistoryService } from './wrangler/wrangler-history.service';
-import { WranglerOperatorController } from './wrangler/wrangler-operator.controller';
-import { WranglerOperatorService } from './wrangler/wrangler-operator.service';
 import { WranglerService } from './wrangler/wrangler.service';
 
 @Module({
   imports: [PrismaModule, BillingModule, NotificationsModule, ChatModule],
-  controllers: [OperationsController, WranglerController, WranglerOperatorController],
-  providers: [ExecutionAutopilotService, WranglerService, WranglerHistoryService, WranglerOperatorService],
-  exports: [ExecutionAutopilotService, WranglerService, WranglerHistoryService, WranglerOperatorService],
+  controllers: [OperationsController, WranglerController],
+  providers: [ExecutionAutopilotService, WranglerService, WranglerHistoryService],
+  exports: [ExecutionAutopilotService, WranglerService, WranglerHistoryService],
 })
 export class OperationsModule {}
