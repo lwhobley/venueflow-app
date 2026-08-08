8e7c62b5cde3823a3cd24e4bc83499bbbf1f7061
lwhobley <lwhobley@uh.edu>
2026-08-08 05:37:32 -0500

    Register Wrangler controller and service

--- packages/api/src/modules/operations/operations.module.ts
+++ packages/api/src/modules/operations/operations.module.ts
@@ -4,11 +4,13 @@ import { PrismaModule } from '../../prisma/prisma.module';
 import { BillingModule } from '../../billing/billing.module';
 import { ChatModule } from '../chat/chat.module';
 import { ExecutionAutopilotService } from './execution-autopilot.service';
+import { WranglerController } from './wrangler/wrangler.controller';
+import { WranglerService } from './wrangler/wrangler.service';
 
 @Module({
   imports: [PrismaModule, BillingModule, ChatModule],
-  controllers: [OperationsController],
-  providers: [ExecutionAutopilotService],
-  exports: [ExecutionAutopilotService],
+  controllers: [OperationsController, WranglerController],
+  providers: [ExecutionAutopilotService, WranglerService],
+  exports: [ExecutionAutopilotService, WranglerService],
 })
 export class OperationsModule {}
