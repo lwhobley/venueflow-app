bf1989657d31c932e3c10e968ce419de67132092
lwhobley <lwhobley@uh.edu>
2026-08-08 06:46:51 -0500

    Allow Wrangler to execute floor actions

--- packages/api/src/modules/operations/operations.module.ts
+++ packages/api/src/modules/operations/operations.module.ts
@@ -3,12 +3,13 @@ import { OperationsController } from './operations.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { BillingModule } from '../../billing/billing.module';
 import { ChatModule } from '../chat/chat.module';
+import { FloorModule } from '../floor/floor.module';
 import { ExecutionAutopilotService } from './execution-autopilot.service';
 import { WranglerController } from './wrangler/wrangler.controller';
 import { WranglerService } from './wrangler/wrangler.service';
 
 @Module({
-  imports: [PrismaModule, BillingModule, ChatModule],
+  imports: [PrismaModule, BillingModule, ChatModule, FloorModule],
   controllers: [OperationsController, WranglerController],
   providers: [ExecutionAutopilotService, WranglerService],
   exports: [ExecutionAutopilotService, WranglerService],
