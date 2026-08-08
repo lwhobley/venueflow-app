db2025bb4588c44ba37affb85ef0ca1ec1ec68d0
lwhobley <lwhobley@uh.edu>
2026-08-08 07:42:16 -0500

    Wire notifications into Wrangler actions

--- packages/api/src/modules/operations/operations.module.ts
+++ packages/api/src/modules/operations/operations.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { OperationsController } from './operations.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { BillingModule } from '../../billing/billing.module';
+import { NotificationsModule } from '../../notifications/notifications.module';
 import { ChatModule } from '../chat/chat.module';
 import { ExecutionAutopilotService } from './execution-autopilot.service';
 import { WranglerController } from './wrangler/wrangler.controller';
 import { WranglerService } from './wrangler/wrangler.service';
 
 @Module({
-  imports: [PrismaModule, BillingModule, ChatModule],
+  imports: [PrismaModule, BillingModule, NotificationsModule, ChatModule],
   controllers: [OperationsController, WranglerController],
   providers: [ExecutionAutopilotService, WranglerService],
   exports: [ExecutionAutopilotService, WranglerService],
