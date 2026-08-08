4474095440c904bfad4df3c916d62ef72aa12604
lwhobley <lwhobley@uh.edu>
2026-08-08 08:10:51 -0500

    Register Wrangler AI operator

--- packages/api/src/modules/operations/operations.module.ts
+++ packages/api/src/modules/operations/operations.module.ts
@@ -7,12 +7,14 @@ import { ChatModule } from '../chat/chat.module';
 import { ExecutionAutopilotService } from './execution-autopilot.service';
 import { WranglerController } from './wrangler/wrangler.controller';
 import { WranglerHistoryService } from './wrangler/wrangler-history.service';
+import { WranglerOperatorController } from './wrangler/wrangler-operator.controller';
+import { WranglerOperatorService } from './wrangler/wrangler-operator.service';
 import { WranglerService } from './wrangler/wrangler.service';
 
 @Module({
   imports: [PrismaModule, BillingModule, NotificationsModule, ChatModule],
-  controllers: [OperationsController, WranglerController],
-  providers: [ExecutionAutopilotService, WranglerService, WranglerHistoryService],
-  exports: [ExecutionAutopilotService, WranglerService, WranglerHistoryService],
+  controllers: [OperationsController, WranglerController, WranglerOperatorController],
+  providers: [ExecutionAutopilotService, WranglerService, WranglerHistoryService, WranglerOperatorService],
+  exports: [ExecutionAutopilotService, WranglerService, WranglerHistoryService, WranglerOperatorService],
 })
 export class OperationsModule {}
